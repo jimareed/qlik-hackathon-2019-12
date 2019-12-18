c70df3213bd44d8643d54b29b7e03dd30d6189ad
Jim Reed <jim.reed@qlik.com>
2019-12-18 09:24:53 -0500

    preliminary webhook implementation
    preliminary webhook implementation

--- webhookHandler.go
+++ webhookHandler.go
@@ -11,7 +11,24 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Print("Github Event: ")
 	log.Print(event)
+
+	apiKey := ""
+	tenantUrl := ""
 	
+	items, err := getItem(apiKey, tenantUrl, "Bugs")
+	if err != nil {
+		log.Print("Get Item error") 
+	} else {
+		for _, i := range items.Data {
+			log.Print("reloading " + i.Name)
+			_, err := reload(apiKey, tenantUrl, i.ResourceId)
+			if err != nil {
+				log.Print("Reload error") 
+			}
+		}
+	}
+
+
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 }
